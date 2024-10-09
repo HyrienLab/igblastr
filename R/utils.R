3de8e8b16edb9b51b71f0076394dc8dcf1e334aa
Hervé Pagès <hpages.on.github@gmail.com>
2024-10-09 08:03:35 -0700

    small improvements to download_IMGT_germline_sequences()

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
-.IMGT_V_QUEST_REFERENCE_DIRECTORY <-
-    "https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory"
+.IMGT_VQUEST_REFERENCE_DIRECTORY <-
+    "https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/"
 
 .IG_FILES <- paste0("IG",
     c("HV", "HD", "HJ", "KV", "KJ", "LV", "LJ"), ".fasta")
@@ -7,44 +7,66 @@
 .TR_FILES <- paste0("TR",
     c("AV", "AJ", "BV", "BD", "BJ", "DV", "DD", "DJ", "GV", "GJ"), ".fasta")
 
-url_exists <- function(url)
+.url_exists <- function(url)
 {
-    code <- HEAD(url)$status_code
-    code >= 200L && code < 300L
+    response <- try(HEAD(url), silent=TRUE)
+    if (inherits(response, "try-error"))
+        stop(as.character(response), "  Please check your internet connection.")
+    response$status_code != 404L
+}
+
+.get_IMGT_VQUEST_orgdir_url <- function(organism)
+{
+    if (!isSingleString(organism))
+        stop(wmsg("'organism' must be a single string"))
+    if (grepl("^\\s*$", organism))
+        stop(wmsg("'organism' contains only whitespace characters"))
+    organism <- chartr(" ", "_", organism)
+    orgdir_url <- paste0(.IMGT_VQUEST_REFERENCE_DIRECTORY, organism, "/")
+    if (!.url_exists(orgdir_url))
+        stop(organism, ": no such organism\n  ",
+             wmsg("See ", .IMGT_VQUEST_REFERENCE_DIRECTORY, " for ",
+                  "the list of supported organisms."))
+    orgdir_url
 }
 
 download_IMGT_germline_sequences <-
     function(organism="Homo_sapiens", subdir=c("IG", "TR", "both"),
-             destdir=".", method, quiet=FALSE)
+             dry.run=FALSE, destdir=".", method, quiet=FALSE)
 {
-    if (!isSingleString(organism))
-        stop(wmsg("'organism' must be a single string"))
+    orgdir_url <- .get_IMGT_VQUEST_orgdir_url(organism)
     subdir <- match.arg(subdir)
+    if (!isTRUEorFALSE(dry.run))
+        stop(wmsg("'dry.run' must be TRUE or FALSE"))
     if (!isSingleString(destdir))
         stop(wmsg("'destdir' must be a single string"))
     if (!dir.exists(destdir))
         stop(wmsg("'destdir' must be the path to an existing directory"))
+
     if (subdir == "both") {
         downloaded_IG_files <- download_IMGT_germline_sequences(organism,
-                                   subdir="IG", destdir=destdir, quiet=quiet)
+                                   subdir="IG", dry.run=dry.run,
+                                   destdir=destdir, method=method, quiet=quiet)
         downloaded_TR_files <- download_IMGT_germline_sequences(organism,
-                                   subdir="TR", destdir=destdir, quiet=quiet)
+                                   subdir="TR", dry.run=dry.run,
+                                   destdir=destdir, method=method, quiet=quiet)
         downloaded_files <- c(downloaded_IG_files, downloaded_TR_files)
     } else {
         files <- if (subdir == "IG") .IG_FILES else .TR_FILES
-        subdir_url <- paste(.IMGT_V_QUEST_REFERENCE_DIRECTORY,
-                            organism, subdir, sep="/")
+        subdir_url <- paste0(orgdir_url, subdir, "/")
         downloaded_files <- character(0)
         for (file in files) {
-            file_url <- paste0(subdir_url, "/", file)
+            file_url <- paste0(subdir_url, file)
             ## Not all organisms have all the IG or TR files.
-            if (url_exists(file_url)) {
-                destfile <- file.path(destdir, file)
-                download.file(file_url, destfile, method, quiet)
+            if (.url_exists(file_url)) {
+                if (!dry.run) {
+                    destfile <- file.path(destdir, file)
+                    download.file(file_url, destfile, method, quiet)
+                }
                 downloaded_files <- c(downloaded_files, file)
             }
         }
     }
-    invisible(downloaded_files)
+    if (dry.run) downloaded_files else invisible(downloaded_files)
 }
 
