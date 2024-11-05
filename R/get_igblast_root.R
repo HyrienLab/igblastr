94bb7728e3c62a7b3e1693f210fdd51e719857e0
Hervé Pagès <hpages.on.github@gmail.com>
2024-11-04 22:18:21 -0800

    add igblast_version() and igblast_info()

--- R/get_igblast_root.R
+++ R/get_igblast_root.R
@@ -81,7 +81,7 @@
     ## We ignore the returned path. Only purpose is to check that the
     ## igblastn and igblastp executables work.
     .make_igblast_cmd_path(igblast_root, cmd="igblastn", OS=OS)
-    .make_igblast_cmd_path(igblast_root, cmd="igblastn", OS=OS)
+    .make_igblast_cmd_path(igblast_root, cmd="igblastp", OS=OS)
     igblast_root
 }
 
@@ -285,5 +285,59 @@ get_igblast_exe <- function(cmd=c("igblastn", "igblastp"))
     igblast_root <- get_igblast_root()
     cmd <- match.arg(cmd)
     .make_igblast_cmd_path(igblast_root, cmd=cmd)
+}
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### igblast_version() and igblast_info()
+###
+
+.extract_version_from_cmd_output <- function(output)
+{
+    sub("^igblastn: *", "", output[[1L]])
+}
+
+### Exported!
+igblast_version <- function()
+{
+    igblastn_exe <- get_igblast_exe("igblastn")
+    out <- system2(igblastn_exe, "-version", stdout=TRUE)
+    .extract_version_from_cmd_output(out)
+}
+
+### Exported!
+print.igblast_info <- function(x, ...)
+{
+    x <- lapply(x, function(x) paste(x, collapse="; "))
+    x <- paste0(names(x), ": ", as.character(x))
+    cat(x, sep="\n")
+}
+
+### Exported!
+igblast_info <- function()
+{
+    igblast_root <- get_igblast_root()
+    OS_arch <- get_OS_arch()
+    OS <- OS_arch[["OS"]]
+    igblastn_exe <- .make_igblast_cmd_path(igblast_root, cmd="igblastn", OS=OS)
+    igblastn_version <- system2(igblastn_exe, "-version", stdout=TRUE)
+    version <- .extract_version_from_cmd_output(igblastn_version)
+    #igblastp_exe <- .make_igblast_cmd_path(igblast_root, cmd="igblastp", OS=OS)
+    #igblastp_version <- system2(igblastp_exe, "-version", stdout=TRUE)
+    build <- igblastn_version[[2L]]  # should be same as igblastp_version[[2L]]
+    build <- sub("^ *Package: ", "", build)
+
+    ans <- list(
+        igblast_root=igblast_root,
+        Version=version,
+        `OS/arch`=paste(OS_arch, collapse="/"),
+        Build=build
+        #igblastn_exe=igblastn_exe,
+        #igblastn_version=igblastn_version[[1L]],
+        #igblastp_exe=igblastp_exe,
+        #igblastp_version=igblastp_version[[1L]]
+    )
+    class(ans) <- "igblast_info"
+    ans
 }
 
